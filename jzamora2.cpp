ff764d3bed5cf74da2f9117c8c77a51ca2a727ff
Jose Zamora <123494107+cosmo-novah@users.noreply.github.com>
2023-02-22 09:50:04 -0800

    Add files via upload

---
+++ jzamora2.cpp
@@ -0,0 +1,21 @@
+//program: walk.cpp
+//author: Jose Zamora //
+//date: 02/21/2022
+
+#ifndef _JOSE_H_
+#define _JOSE_H_
+#include "fonts.h"
+
+extern void test_text (int yres)
+{
+    Rect r;
+    unsigned int c = 0x00ffff44;
+	r.bot = yres - 50;
+	r.left = 100;
+	r.center = 1;
+	ggprint8b(&r, 16, c, "Some Text");
+    ggprint8b(&r, 16, c, "As a text");
+
+}
+
+#endif
