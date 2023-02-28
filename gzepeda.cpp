fd07bb5da7a9249e1f7f98861c1a5e3f716694f5
Geno  Zepeda <gzepeda09@gmail.com>
2023-02-27 20:47:00 -0800

    updated my code
    updated my code and the .gitignore

--- gzepeda.cpp
+++ gzepeda.cpp
@@ -8,13 +8,15 @@ Purpose: Draws text
 
 */
 #include "fonts.h"
-
+#include <GL/glx.h>
 
 extern void newText(int yres){
 	Rect r;
 	//
+	glClear(GL_COLOR_BUFFER_BIT);
 	r.bot = yres - 20;
 	r.left = 100;
 	r.center = 0;
 	ggprint8b(&r, 16, 0x00ff0000, "Test");
+
 }
